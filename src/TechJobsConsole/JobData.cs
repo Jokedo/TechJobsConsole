3ad44604eb0601043ae9c368112c17d68e6c6ce3
Jonathan Dowd <Jdowd20@gmail.com>
2021-12-11 12:38:00 -0500

    completed job and program changes

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -58,6 +58,26 @@ namespace TechJobsConsole
             return jobs;
         }
 
+        public static List<Dictionary<string, string>> FindByValue(string value)
+        {
+            LoadData();
+
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+
+            foreach (Dictionary<string, string> row in AllJobs)
+            {
+                foreach(KeyValuePair<string, string> keyValuePair in row)
+                {
+                    if (keyValuePair.Value.ToLower().Contains(value.ToLower()))
+                    {
+                        jobs.Add(row);
+                    }
+                }
+            }
+            return jobs;
+        }
+
+
         /*
          * Load and parse data from job_data.csv
          */
